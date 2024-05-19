f9a997d77e013fa3aeb15cdd5d0ed80eb63b7a07
Umar Jamshed <128473349+UmarJamshed947@users.noreply.github.com>
2024-05-20 01:14:32 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_acadmy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_acadmy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/header.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+
+import '../res/assets.dart';
+
+class Header extends StatelessWidget {
+  const Header({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 500.0,
+      decoration: BoxDecoration(
+        color: Colors.grey.shade100,
+        image: DecorationImage(
+          image: AssetImage(Assets.instructor),
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(
+            Colors.black.withOpacity(0.7),
+            BlendMode.srcATop,
+          ),
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(height: 40.0),
+          Text(
+            "Welcome to Flutter Academy",
+            style: Theme.of(context).textTheme.displayMedium?.copyWith(
+              color: Colors.white,
+            ),
+          ),
+          Text(
+            "Your one stop education hub to learn Flutter.",
+            style: Theme.of(context)
+                .textTheme
+                .headlineSmall
+                ?.copyWith(color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+}
